7845b82812fe6a3b66649e096ddbf87b52bbd58b
Elisa Miranda Escalante <elisamirandaescalante@MacBook-Air-de-Elisa-2.local>
2024-08-18 18:12:13 -0600

    First commit

---
+++ CV_Elisa_Miranda_Escalante.py
@@ -0,0 +1,125 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="CV Elisa Miranda Escalante",
+    page_icon="👩🏻‍💼",
+)
+ 
+st.image("./img/elisa.png")
+
+
+st.write("# Elisa Miranda Escalante")
+st.markdown(
+    """
+    Guadalajara, Jalisco | 33-3150-9204 | elisame00@gmail.com
+    |---|---|---|
+    """
+)
+
+st.markdown(
+    """
+## PERFIL PROFESIONAL
+
+Buscando mejorar el 1% cada día, soy Lic. en Administración y Mercadotecnia de la Universidad Panamericana, acutalmente realizando Finanzas como doble titulación. Emprendedora y hambrienta de aprendizaje. Participé en distintos grupos y competencias, al igual que clases extracurriculares. Amplia de conocimientos en distintas áreas, lista para lo nuevo y siempre firme con mis valores. ¡Emocionada por empezar!
+
+---
+    """
+)
+
+
+st.markdown(
+    """
+## EXPERIENCIA
+
+#### Amenity colchones y hogar, GDL - *Asistente, ventas, atención al cliente*
+
+Noviembre de 2019 – Junio 2021 / Enero 2022 – septiembre 2023 / Negocio familiar
+* Ventas en piso y en línea (Instagram, Facebook y Mercado Libre).
+* Atención con el cliente.
+* Capacitación: impartí una sesión de “Presence” (Amy Cudy) para
+nuestras colaboradoras.
+* Implementación de venta en Mercado Libre.
+* Revisión de rendimiento de colaboradoras.
+
+#### Muebles BEIO - *Emprendimiento*
+Noviembre de 2020 – Diciembre 2021
+
+* Habilidades de venta en línea en Facebook. Creación de campañas en redes sociales.
+
+#### José Luis Reyes – *Project Manager*
+Noviembre de 2020 – Enero 2022
+
+* Gestión de proyectos, al igual que creación y gestión de campañas de marketing en redes sociales: Curso de Inteligencia Emocional para Líderes, Programa de Liderazgo, Campaña de Pre-lanzaminento del libro “Empresa Utópica”, campaña de recolección de fondos de NAEA.
+* Community Manager de las redes sociales de José Luis Reyes.
+
+#### Productos Tajín – *Becaria de marketing para LATAM y Otros Países*
+Septiembre 2023 – ACTUALMENTE
+
+* Implementación de estrategia de expansión hacia nuevos países (Latinoamérica, Europa, Australia, Sudáfrica).
+* Asistencia y alineación con equipos de los distintos países.
+* Revisión, aprobación y gestión de la estrategia de medios digitales.
+* Revisión de presupuestos y ventas.
+* Gestión de creación de diseños para distintos materiales de marketing.
+* Gestión de compra de materiales de marketing.
+* Gestión de eventos internos.
+* Reestructuración y/o creación de procesos (administrativos).
+* Actualización de la página web.
+----
+    """
+)
+
+st.markdown(
+    """
+### HABILIDADES FUNDAMENTALES
+
+* Uso de herramientas para la organización (Google Calendar, Drive, AirTable, Slack, etc).
+* Excel avanzado (programar en Visual Basic), Word y Power point.
+* Estudio en R
+* Inglés C1
+* SAP básico.
+* Finanzas, administración, mercadotecnia.
+* Ortografía, mecanografía y escritura. Capacidad de síntesis.
+* Coordinación y comunicación intra e inter cultural.
+-----
+    """
+)
+
+st.markdown(
+    """
+
+### EDUCACIÓN 
+
+#### DiME - Desarrollo en Métodos de Enseñanzas - *Preparatoria*
+
+AGOSTO 2015-2018
+
+#### Universidad Panamericana - *Lic. Administración y mercadotecnia / Segunda titulación en Finanzas*
+
+AGOSTO 2018 - ACTUALIDAD
+
+* Formé parte del grupo estudiantil ENACTUS (emprendimiento social). ○ Participé en la competencia del Hult Prize.
+* Becaria en el departamento de Arte y Cultura.
+* Curso de oratoria
+
+#### Kühne Logistics University - *Study abroad program*
+
+AGOSTO 2022 - DICIEMBRE 2022
+
+----
+    """
+)
+st.markdown (
+    """
+### OTROS INTERESES
+
+* Lectura
+* Creación e implementación de
+estrategias
+* Crecimiento del valor de una marca
+* Análisis de datos.
+* Búsqueda constante de aprendizaje (cursos, artículos, libros, etc).
+* Optimización de procesos.
+* Arte, música y naturaleza.
+
+    """
+)
